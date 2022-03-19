968d788f9356b9c9e366bbdfa7ca0f1c479edb34
Casper Steengaard <casper.aakviststeengaard@gmail.com>
2022-03-19 17:14:53 +0100

    initial commit

---
+++ src/main/java/com/example/krakdemo/KrakDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.krakdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrakDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrakDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/krakdemo/KrakDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.krakdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrakDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
